1ecf416ce7d1e23329c9a8349491b4dcbf6059d6
Jean-Philippe Léveillé <jpleveille@gmail.com>
2018-11-19 22:06:33 -0500

    Release 0.2.3

--- src/Application.php
+++ src/Application.php
@@ -11,7 +11,7 @@ use Webgraphe\Phollow\Documents;
 class Application
 {
     /** @var string */
-    const VERSION = '0.2.2';
+    const VERSION = '0.2.3';
     /** @var string */
     const DEFAULT_ROUTE_IP = '0.0.0.0';
 
